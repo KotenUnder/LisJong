f15e022bacb23742983b97c7de1856cbd8a30ec9
KotenUnder <koten_under@yahoo.co.jp>
2023-07-16 10:48:12 +0900

    セットアップ関連

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lisjong',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='KotenUnder',
+    author_email='',
+    description=''
+)
